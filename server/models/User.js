2dea73c51cd082b2e6b4520c2fc2308fa39df10d
KunalCodeCraft02 <kunalbodkhe080@gmail.com>
2026-06-27 18:31:55 +0530

    fix: remove duplicate email index in User schema

--- server/models/User.js
+++ server/models/User.js
@@ -48,8 +48,6 @@ const userSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
-userSchema.index({ email: 1 });
-
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(12);
